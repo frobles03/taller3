92a7023da52207fb62f0ac9d0590798ac59cec7e
f.robles03 <f.robles03@ufromail.cl>
2022-11-11 16:05:11 -0300

    Se agrego el avance el proyecto

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,18 +1,49 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div class="Trabajos">
+    <h1>bienvenido al taller 3</h1>
+    <div class="post" v-for="product in products" v-bind:key="product._id">
+      <!-- <strong> <router-link to="/trabajo">{{product.title}}</router-link>  </strong> :{{ product.salary }} -->
+
+      <li>
+        
+          <strong>nombre del producto: </strong> {{ product.name }}
+        
+
+        
+          <strong> fecha de creacion</strong> {{ product.createdAt }}
+      
+      
+
+        <div v-for="(images) in product.images" v-bind:key="images">
+
+          <img :src="images" />
+        </div>
+
+      </li>
+    </div>
+
   </div>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
-
+// @ is an alias to /src    lista de los trabajos y que si se apreta uno de esos traabajos ver que aplicantes 
+// import HelloWorld from '@/components/HelloWorld.vue'
+import axios from 'axios'
 export default {
-  name: 'HomeView',
-  components: {
-    HelloWorld
+  name: 'productsView',
+  data() {
+    return {
+      products: []
+    }
+  },
+
+  mounted() {
+    let vue = this;
+    axios.get('http://170.239.85.65:4000/products')
+      .then(function (response) {
+        vue.products = response.data;
+        console.log(vue.products)
+      })
   }
 }
 </script>
